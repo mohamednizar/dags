1e9f9f7a9402502cab97dd96dfc43ce611ec4e1a
Mohamed Nizar <nizarucsc@gmail.com>
2020-11-28 00:24:00 +0530

    update dag task

--- dynamic_superset_queries.py
+++ dynamic_superset_queries.py
@@ -7,15 +7,27 @@ from airflow.hooks.mysql_hook import MySqlHook
 from airflow.utils.dates import days_ago
 import logging
 
-dag = DAG(
-    dag_id='dynamic_superset_queries',
-    default_args={"owner": "airflow", "provide_context" : True},
-    start_date=days_ago(1),
-    schedule_interval="@once"
-)
+default_args = {
+    "owner": "airflow",
+    "provide_context": True
+}
 
 
-def create_or_update_table(**context):
+def generate_dags_for_queries(**context):
+    table_name = format(context["dag_run"].conf["table_name"])
+    dag_name = 'dynamic_superset_queries_dag_generator_{}'.table_name
+    dag = DAG(
+        dag_name,
+        schedule_interval=timedelta(minutes=5),
+        start_date=days_ago(1),
+        default_args=default_args
+    )
+    dag_task = PythonOperator(task_id="running_queries_{}".table_name, python_callable=create_or_update_table, dag=dag)
+    globals()[dag_name] = dag
+    return dag
+
+
+def insert_or_update_table(**context):
     """
     access the  payload params passed to the DagRun conf attribute.
     :param context: The execution context
@@ -42,4 +54,14 @@ def create_or_update_table(**context):
         logging.exception(e3)
 
 
-run_this = PythonOperator(task_id="run_this", python_callable=create_or_update_table, dag=dag)
+dag = DAG(
+    dag_id='dynamic_superset_queries_dag_generator',
+    default_args={"owner": "airflow", "provide_context": True},
+    start_date=days_ago(1),
+    schedule_interval="@once"
+)
+
+dags_creator_task = PythonOperator(
+    task_id="dags_creator_task",
+    python_callable=generate_dags_for_queries,
+)
