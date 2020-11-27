3ac4bc98ed514fc711588b1dd8ab78277d88e007
Mohamed Nizar <nizarucsc@gmail.com>
2020-11-27 20:36:56 +0530

    formate and handle exception

--- dynamic_superset_queries.py
+++ dynamic_superset_queries.py
@@ -1,38 +1,37 @@
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import DAG
-from datetime import datetime , timedelta
+from datetime import datetime, timedelta
 from airflow.operators.mysql_operator import MySqlOperator
 from airflow.hooks.mysql_hook import MySqlHook
 from airflow.utils.dates import days_ago
+import logging
 import os
 
 dag = DAG(
-     dag_id='dynamic_superset_queries',
-     default_args={"owner": "airflow"},
-     start_date=days_ago(1),
-     schedule_interval="@once",
+    dag_id='dynamic_superset_queries',
+    default_args={"owner": "airflow"},
+    start_date=days_ago(1),
+    schedule_interval="@once",
 )
 
-def create_or_update_table(**context):
-    sql = context["drag_run"].conf["sql"]
-    table_name = context["drag_run"].conf["table_name"]
-    print('table name: '.table_name) 
-    src = MysqlHook(mysql_conn_id='openemis')
-    dest = MysqlsHook(mysql_conn_id='analytics')
-    src_conn = src.get_conn()
-    cursor = src_conn.cursor()
-    dest_conn = dest.get_conn()
-    dest_cursor = dest_conn.cursor()
-    cursor.execute(query_params.sql)
-    dest.truncate(table=query_params.table_name)
-    dest.insert_rows(table=query_params.table_name, rows=cursor)
-    
-    
-run_this =  PythonOperator(task_id="run_this", python_callable=create_or_update_table, dag=dag)
 
-# bash_task = BashOperator(
-#     task_id="bash_task",
-#     env={'sql': '{{ dag_run.conf["sql"] if dag_run else "" }}','table_name': '{{ dag_run.conf["table_name"] if dag_run else "" }}'},
-#     provide_context=True, 
-#     dag=dag,
-# )
+def create_or_update_table(**context):
+    try:
+        sql = context["drag_run"].conf["sql"]
+        table_name = context["drag_run"].conf["table_name"]
+        logging.info('connecting to source')
+        src = MySqlHook(mysql_conn_id='openemis')
+        logging.info('connecting to destination')
+        dest = MySqlHook(mysql_conn_id='analytics')
+        src_conn = src.get_conn()
+        cursor = src_conn.cursor()
+        dest_conn = dest.get_conn()
+        cursor.execute(sql)
+        dest.truncate(table=table_name)
+        dest.insert_rows(table=table_name, rows=cursor)
+    except Exception as e3:
+        logging.error('Dag failed , please refer the logs more details')
+        logging.exception(e3)
+
+
+run_this = PythonOperator(task_id="run_this", python_callable=create_or_update_table, dag=dag)
