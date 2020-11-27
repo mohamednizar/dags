fba40abc87248df4230099e78ce414d49ec1da2a
Mohamed Nizar <nizarucsc@gmail.com>
2020-11-28 02:28:38 +0530

    Update dynamic_superset_queries_dag_generator.py

--- dynamic_superset_queries_dag_generator.py
+++ dynamic_superset_queries_dag_generator.py
@@ -63,7 +63,7 @@ def generate_dags_for_queries(**context):
         with dag:
             dag_task = PythonOperator(task_id=task_name, python_callable=insert_or_update_table,
                                       dag=dag)
-        globals()[dag_name] = dag
+        globals()[dag_id] = dag
         return dag
     except Exception as e3:
         logging.error('Dag creation failed , please refer the logs more details')
