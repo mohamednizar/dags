f33e11ef55ab0cdbfb9335b8b50befca5c585901
Mohamed Nizar <nizarucsc@gmail.com>
2020-11-27 20:56:55 +0530

    add debug points

--- dynamic_superset_queries.py
+++ dynamic_superset_queries.py
@@ -6,6 +6,7 @@ from airflow.hooks.mysql_hook import MySqlHook
 from airflow.utils.dates import days_ago
 import logging
 import os
+import pprint
 
 dag = DAG(
     dag_id='dynamic_superset_queries',
@@ -15,10 +16,12 @@ dag = DAG(
 )
 
 
-def create_or_update_table(**context):
+def create_or_update_table(**kwargs):
     try:
-        sql = context["drag_run"].conf["sql"]
-        table_name = context["drag_run"].conf["table_name"]
+        logging.info('trying the task')
+        pprint(kwargs);
+        sql = kwargs["drag_run"].conf["sql"]
+        table_name = kwargs["drag_run"].conf["table_name"]
         logging.info('connecting to source')
         src = MySqlHook(mysql_conn_id='openemis')
         logging.info('connecting to destination')
@@ -32,6 +35,7 @@ def create_or_update_table(**context):
     except Exception as e3:
         logging.error('Dag failed , please refer the logs more details')
         logging.exception(e3)
+        logging.exception(kwargs)
 
 
 run_this = PythonOperator(task_id="run_this", python_callable=create_or_update_table, dag=dag)
