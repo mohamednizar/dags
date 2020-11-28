ecaa7948a1941cc803a6c536dad3749214a2ddc6
Mohamed Nizar <nizarucsc@gmail.com>
2020-11-28 19:15:14 +0530

    Update dynamic_superset_queries_dag_generator.py

--- dynamic_superset_queries_dag_generator.py
+++ dynamic_superset_queries_dag_generator.py
@@ -60,9 +60,9 @@ def generate_dags_for_queries(**context):
     """
     try:
         table_name = format(context["dag_run"].conf["table_name"]).lower()
-        dag_name = f"dynamic_superset_queries_dag_{table_name}"
+        dag_id = f"dynamic_superset_queries_dag_{table_name}"
         new_dag = DAG(
-            dag_name,
+            dag_id,
             default_args={"owner": "airflow"},
             start_date=days_ago(1),
             schedule_interval=timedelta(minutes=5),
@@ -76,7 +76,8 @@ def generate_dags_for_queries(**context):
                 python_callable=insert_or_update_table)
 
         # Try to place the DAG into globals(), which doesn't work
-        globals()[dag_name] = new_dag
+        globals()[dag_id] = new_dag
+        return new_dag
     except Exception as e3:
         logging.error('Dag creation failed , please refer the logs more details')
         logging.exception(context)
