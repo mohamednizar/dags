a111ff8b310c4880e8d1e24cc4daf4e37d7bc45f
Mohamed Nizar <nizarucsc@gmail.com>
2020-11-30 13:12:06 +0530

    Update superset_dags_gen.py

--- superset_dags_gen.py
+++ superset_dags_gen.py
@@ -25,11 +25,10 @@ login_form = s.post(f"{base_url}/login")
 
 # get Cross-Site Request Forgery protection token
 soup = BeautifulSoup(login_form.text, 'html.parser')
-#csrf_token = soup.find('input', {'id': 'csrf_token'})['value']
-logging.info(login_form)
+csrf_token = soup.find('input', {'id': 'csrf_token'})['value']
 
 # login the given session
-s.post(login_form, data=dict(username=superset_username, password=superset_password))
+s.post(login_form, data=dict(username=superset_username, password=superset_password, csrf_token=csrf_token))
 
 # fetch all saved queries
 saved_queries = s.get(f"{base_url}/savedqueryviewapi/api/read").text
@@ -48,7 +47,6 @@ START = DummyOperator(
 END = DummyOperator(
     task_id='END'
 )
-
 
 
 def insert_or_update_table(**kwargs):
