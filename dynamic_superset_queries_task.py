5d21714802fcaa2be12a534d4fa1156d17bbc9ab
Mohamed Nizar <nizarucsc@gmail.com>
2020-11-30 17:59:52 +0530

    Update dynamic_superset_queries_task.py

--- dynamic_superset_queries_task.py
+++ dynamic_superset_queries_task.py
@@ -1,5 +1,12 @@
+import requests
+import os
+import json
+import pprint
+from bs4 import BeautifulSoup as bs
+from bs4 import Comment
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.models import DAG
 from datetime import datetime, timedelta
 from airflow.operators.mysql_operator import MySqlOperator
@@ -7,47 +14,132 @@ from airflow.hooks.mysql_hook import MySqlHook
 from airflow.utils.dates import days_ago
 import logging
 
+superset_url = os.environ['SUPERSET_URL']
+superset_username = os.environ['SUPERSET_USERNAME']
+superset_password = os.environ['SUPERSET_PASSWORD']
+
+
+class UseSupersetApi:
+    def __init__(self, username=None, password=None):
+        self.s = requests.Session()
+        self.base_url = superset_url
+        self._csrf = self._getCSRF(self.url('login/'))
+        self.headers = {'X-CSRFToken': self._csrf, 'Referer': self.url('login/')}
+        # note: does not use headers because of flask_wtf.csrf.validate_csrf
+        # if data is dict it is used as form and ends up empty but flask_wtf checks if data ...
+        self.s.post(self.url('login/'),
+                    data={'username': username, 'password': password, 'csrf_token': self._csrf})
+
+    def url(self, url_path):
+        return self.base_url + url_path
+
+    def get(self, url_path):
+        return self.s.get(self.url(url_path), headers=self.headers)
+
+    def post(self, url_path, data=None, json_data=None, **kwargs):
+        kwargs.update({'url': self.url(url_path), 'headers': self.headers})
+        if data:
+            data['csrf_token'] = self._csrf
+            kwargs['data'] = data
+        if json_data:
+            kwargs['json'] = json_data
+        return self.s.post(**kwargs)
+
+    def _getCSRF(self, url_path):
+        response = self.s.get(self.base_url)
+        soup = bs(response.content, "html.parser")
+        for tag in soup.find_all('input', id='csrf_token'):
+            csrf_token = tag['value']
+        return csrf_token
+
+
 dag = DAG(
     dag_id='dynamic_superset_queries_task',
     default_args={"owner": "airflow", "provide_context": True},
     start_date=days_ago(1),
-    schedule_interval="@once"
+    schedule_interval=timedelta(minutes=10)
+)
+
+START = DummyOperator(
+    task_id='START'
+)
+
+END = DummyOperator(
+    task_id='END'
 )
 
 
-def create_or_update_table(**context):
+def insert_or_update_table(**kwargs):
     """
-    access the  payload params passed to the DagRun conf attribute.
-    :param context: The execution context
-    :type context: dict
-    """
+     access the  payload params passed to the DagRun conf attribute.
+     :param context: The execution context
+     :type context: dict
+     """
     try:
+        table_name = kwargs['schedule_info']['table_name']
+        sql = kwargs['sql']
         logging.info('trying the task')
-        sql = format(context["dag_run"].conf["sql"])
-        table_name = format(context["dag_run"].conf["table_name"])
         logging.info('connecting to source')
-        src = MySqlHook(mysql_conn_id='openemis')
+        src = MySqlHook(mysql_conn_id=kwargs['schedule_info']['schema'])
+        logging.info(f"Remotely received sql of {sql}")
+        logging.info(f"Remotely received sql of {table_name}")
         logging.info('connecting to destination')
-        print("Remotely received value of {} for key=sql".sql)
-        print("Remotely received value of {} for key=table_name".table_name)
         dest = MySqlHook(mysql_conn_id='analytics')
         src_conn = src.get_conn()
         cursor = src_conn.cursor()
-        dest_conn = dest.get_conn()
         cursor.execute(sql)
         dest.insert_rows(table=table_name, rows=cursor, replace=True)
     except Exception as e3:
-        logging.error('Dag failed , please refer the logs more details')
+        logging.error('Table update is failed, please refer the logs more details')
         logging.exception(context)
         logging.exception(e3)
 
 
-def dynamic_task(**context):
-    table_name = format(context["dag_run"].conf["table_name"])
-    task_name = f"create_or_update_{table_name}".lower()
-    create_dynamic_task = PythonOperator(task_id=task_name, python_callable=create_or_update_table, dag=dag)
-    return create_dynamic_task
+def generate_dags_for_queries(dag_id, schedule,  default_args, saved_queries  ):
+    try:
+        logging.info(f"DAG is:{dag_id}")
+        with DAG(dag_id, default_args=default_args, schedule_interval=schedule, catchup=False) as new_dag:
+            task_name = f"running_queries_{table_name}".upper()
+
+            dummy_start = DummyOperator(
+                task_id='dummy_start'
+            )
+
+            dummy_end = DummyOperator(
+                task_id='dummy_end'
+            )
+
+            dag_task = PythonOperator(
+                task_id=task_name,
+                python_callable=insert_or_update_table,
+                op_kwargs=saved_queries,
+                provide_context=True
+            )
+            dummy_start >> dag_task
+            dag_task >> dummy_end
+            logging.info('Task is:{}'.task_name)
+            return new_dag
+    except Exception as e3:
+        logging.error('Dag creation failed , please refer the logs more details')
+        logging.exception(context)
+        logging.exception(e3)
 
 
-with dag:
-    run_this = PythonOperator(task_id="dynamic_task_create", python_callable=dynamic_task, dag=dag)
+superset = UseSupersetApi(superset_username, superset_password)
+saved_queries = superset.get(url_path='/savedqueryviewapi/api/read').text
+number_of_queries = (json.load(saved_queries))
+for n in number_of_queries:
+    saved_query = saved_queries[n]
+    data = json.load(saved_query["extra_json"])
+    table_name = data['schedule_info']['output_table']
+    dag_id = f"saved_queries_update{table_name}".lower()
+
+    default_args = {'owner': 'airflow',
+                    'start_date': data['schedule_info']['start_date'],
+                    'end_date': data['schedule_info']['end_date'],
+                    }
+    schedule = timedelta(minutes=10)
+    globals[dag_id] = generate_dags_for_queries(dag_id,
+                                                schedule,
+                                                default_args,
+                                                saved_queries)
