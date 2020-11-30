eeca0f527679b4fa70ef6e7b24fb664f5f490fac
Mohamed Nizar <nizarucsc@gmail.com>
2020-11-30 17:50:25 +0530

    Update test_dag_pipline.py

--- test_dag_pipline.py
+++ test_dag_pipline.py
@@ -1,41 +1,45 @@
 import requests
+from bs4 import BeautifulSoup as bs
+from bs4 import Comment
+import pprint
 import os
-import json
-import pprint
-from bs4 import BeautifulSoup
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.models import DAG
-from datetime import datetime, timedelta
-from airflow.operators.mysql_operator import MySqlOperator
-from airflow.hooks.mysql_hook import MySqlHook
-from airflow.utils.dates import days_ago
-import logging
-import pprint
 
-superset_host = os.environ['SUPERSET_HOST']
+
+class UseSupersetApi:
+    def __init__(self, username=None, password=None):
+        self.s = requests.Session()
+        self.base_url = os.environ['SUPERSET_URL']
+        self._csrf = self._getCSRF(self.url('login/'))
+        self.headers = {'X-CSRFToken': self._csrf, 'Referer': self.url('login/')}
+        # note: does not use headers because of flask_wtf.csrf.validate_csrf
+        # if data is dict it is used as form and ends up empty but flask_wtf checks if data ...
+        self.s.post(self.url('login/'),
+                    data={'username': username, 'password': password, 'csrf_token': self._csrf})
+
+    def url(self, url_path):
+        return self.base_url + url_path
+
+    def get(self, url_path):
+        return self.s.get(self.url(url_path), headers=self.headers)
+
+    def post(self, url_path, data=None, json_data=None, **kwargs):
+        kwargs.update({'url': self.url(url_path), 'headers': self.headers})
+        if data:
+            data['csrf_token'] = self._csrf
+            kwargs['data'] = data
+        if json_data:
+            kwargs['json'] = json_data
+        return self.s.post(**kwargs)
+
+    def _getCSRF(self, url_path):
+        response = self.s.get(self.base_url)
+        soup = bs(response.content, "html.parser")
+        for tag in soup.find_all('input', id='csrf_token'):
+            csrf_token = tag['value']
+        return csrf_token
+
+
 superset_username = os.environ['SUPERSET_USERNAME']
 superset_password = os.environ['SUPERSET_PASSWORD']
-superset_port = os.environ['SUPERSET_PORT']
-
-# set up session for auth
-s = requests.Session()
-base_url = f"http://{superset_host}:{superset_port}/"
-print(f"{base_url}login")
-login_form = s.post(f"{base_url}login")
-pprint(login_form)
-
-
-
-# get Cross-Site Request Forgery protection token
-soup = BeautifulSoup(login_form.text, 'html.parser')
-csrf_token = soup.find('input', {'id': 'csrf_token'})['value']
-
-# login the given session
-s.post(login_form, data=dict(username=superset_username, password=superset_password, csrf_token=csrf_token))
-
-# fetch all saved queries
-saved_queries = s.get(f"{base_url}savedqueryviewapi/api/read").text
-print(saved_queries)
-
+superset = UseSupersetApi(superset_username, superset_password)
+print(superset.get(url_path='/savedqueryviewapi/api/read'))
